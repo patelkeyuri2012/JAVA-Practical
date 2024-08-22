11cf8ccc449fb5faa15756fed62871c69ebc8b77
Keyuri Patel <patelkeyuri2002@gmail.com>
2024-08-22 22:24:22 +0530

    Collection

--- Collection/MyClass.java
+++ Collection/MyClass.java
@@ -1,7 +1,20 @@
 // create generic class
 public class MyClass<T extends Comparable<T>> {
 
-    
+    public T findMaximum(T[] array) {
+        if (array == null || array.length == 0) {
+            return null; 
+        }
+
+        T max = array[0]; 
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i].compareTo(max) > 0) {
+                max = array[i]; 
+            }
+        }
+        return max;
+    }
 
     public static void main(String[] args) {
        
